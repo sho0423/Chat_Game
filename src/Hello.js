dbdef8d5b5eccb2e96b433824972702fd71820b3
shotaro_okamoto <sho662607004081@gmail.com>
2019-12-27 14:19:57 +0900

    initial

--- src/Hello.js
+++ src/Hello.js
@@ -3,4 +3,12 @@ export class Hello extends React.Component {
   render () {
     return <h1>Hello!</h1>
   }
+
+}
+
+export class World extends React.Component {
+  render () {
+    return <h1>World!</h1>
+  }
+  
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Hello} from './Hello'
+import {World} from './Hello'
 
 ReactDOM.render(
-    <Hello />,
+    <>
+        <Hello />
+        <World />
+    </>,
     document.getElementById('root'))
